03be182f7a4b5edc68b1b7c55b9bc77c10e86ac9
Alexandre <kalexandre@thales.ma>
2020-07-13 14:43:51 +0100

    Init devops project.

--- vagrant/jenkins.sh
+++ vagrant/jenkins.sh
@@ -21,6 +21,25 @@ echo "Installing jenkins"
 sudo apt-get -y install jenkins > /dev/null 2>&1
 sudo service jenkins start
 
+echo "Updating apt-get"
+sudo apt-get -qq update
+
+echo "Installing dependencies"
+sudo apt-get -y install apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common \
+    python3-pip \
+    python-pip \
+    virtualenv \
+    python3-setuptools > /dev/null 2>&1
+
+echo "Adding repository"
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+
+echo "Installing ansible"
+sudo apt-get -y install ansible > /dev/null 2>&1
+
 echo "Installing docker"
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get -qq update
@@ -40,6 +59,11 @@ sudo apt-get -qq update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io > /dev/null 2>&1
 sudo groupadd docker
 sudo usermod -aG docker jenkins
+sudo usermod -aG docker vagrant
+
+echo "Installing docker module for python3 and python2"
+pip3 install docker
+pip install docker
 
 sleep 1m
 
@@ -47,9 +71,10 @@ echo "Installing Jenkins Plugins"
 JENKINSPWD=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
 echo $JENKINSPWD
 
-echo "Please remenber to add jenkins user to group docker and reboot machine..."
+echo "Please remenber to add jenkins & vagrant users to group docker and reboot machine..."
 echo "\
     $ vagrant ssh \
     $ sudo groupadd docker \
-    $ sudo usermod -aG docker jenkins 
+    $ sudo usermod -aG docker jenkins \
+    $ sudo usermod -aG docker vagrant \
     $ sudo reboot"
